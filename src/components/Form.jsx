6677da661ffb37af6578bccc7ae2fb5d96180be1
Esmir Roque Castellano <74161172+castellano17@users.noreply.github.com>
2023-08-17 16:58:55 -0600

    API connection

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import useSelectMoney from "../hooks/useSelectMoney";
 import { monedas } from "../utils/Money";
@@ -23,7 +23,29 @@ const InputSubmit = styled.input`
 `;
 
 const Form = () => {
+  const [criptos, setCriptos] = useState([]);
   const [moneda, SelectMoney] = useSelectMoney("Elige tu moneda", monedas);
+
+  useEffect(() => {
+    const consultAPI = async () => {
+      const URL =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+      const response = await fetch(URL);
+
+      const result = await response.json();
+      const arrayCriptos = result.Data.map((cripto) => {
+        const objet = {
+          id: cripto.CoinInfo.Name,
+          name: cripto.CoinInfo.FullName,
+        };
+
+        return objet;
+      });
+      setCriptos(arrayCriptos);
+    };
+
+    consultAPI();
+  }, []);
 
   return (
     <form>
